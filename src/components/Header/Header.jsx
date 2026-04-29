7dd19ee569f295080c9ddfe337b0c1eaad6b9422
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-04-29 15:54:17 +0600

    navbar and header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <NavLink
           to="/"
           end
-          className="font-medium text-base px-3 py-1"
+          className="font-medium text-base  px-4 py-2"
           style={({ isActive }) =>
             isActive
               ? {
@@ -29,7 +29,7 @@ const Header = () => {
         <NavLink
           to="/products"
           end
-          className="font-medium text-base px-3 py-1"
+          className="font-medium text-base  px-4 py-2"
           style={({ isActive }) =>
             isActive
               ? {
@@ -50,7 +50,7 @@ const Header = () => {
   );
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar border-b border-[#E9E9E9] bg-white ">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -86,8 +86,15 @@ const Header = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{Links}</ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+        <div className="navbar-end gap-4">
+          <a className="btn border-2 border-[#632EE3]">
+            <span className="bg-gradient-to-br from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">
+              Login
+            </span>
+          </a>
+          <button className="btn bg-gradient-to-br from-[#632EE3] to-[#9F62F2] text-white border-0">
+            Register
+          </button>
         </div>
       </div>
     </div>
